c85f69716cf69c01bc4a95b94192dda9cdf00ab4
ars <joker5916@riseup.net>
2021-05-21 17:07:12 +0300

    Updates

--- _nuxt/static/1621605415/fi/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdedmoroz\u002F_nuxt\u002Fstatic\u002F1621605415",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffi",config:{_app:{basePath:"\u002Fdedmoroz\u002F",assetsPath:"\u002Fdedmoroz\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"940f6f84"}}}}(null));
