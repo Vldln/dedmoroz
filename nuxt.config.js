630e0483c10fc0f7a7838b24e56ad5f1e8d41ffd
ars <joker5916@riseup.net>
2021-05-21 16:51:23 +0300

    Смена названия

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'dedmoroz',
+    title: 'Feast after plague',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
