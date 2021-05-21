09aab1b466dbbd74c5b2a718461fe2d61f2e18d4
ars <joker5916@riseup.net>
2021-05-21 17:09:18 +0300

    Правки

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/dedmoroz/',
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Feast after plague',
